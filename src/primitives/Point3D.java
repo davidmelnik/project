07675b5d56d8617fb6c55bbfd64c3d0f2acf5d63
david melnik <davideli@g.jct.ac.il>
2021-03-14 17:29:01 +0200

    Initial commit

---
+++ src/primitives/Point3D.java
@@ -0,0 +1,15 @@
+package primitives;
+
+public class Point3D {
+
+    Coordinate x;
+    Coordinate y;
+    Coordinate z;
+
+    public Point3D(Coordinate x, Coordinate y, Coordinate z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    
+}
